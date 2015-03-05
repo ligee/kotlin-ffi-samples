f97b8dc4c923ff025d241fa708b2c942838586e5
ligee <ilya.chernikov@jetbrains.com>
2015-03-05 22:12:54 +0100

    Some more experiments with callbacks, hopefully acceptable solution found

--- ffi-bench/src/jnrcallback.kt
+++ ffi-bench/src/jnrcallback.kt
@@ -13,11 +13,13 @@ public class KTCallBack() {
 
     public trait Callback_int_int {
         jnr.ffi.annotations.Delegate
-        public fun callback(param: Int): Int
+        public open fun callback(param: Int): Int
     }
 
     class object {
 
+        // conversion doesn't work
+        // \todo find out the reason and try the conversion variant
         ToNativeConverter.ToNative(nativeType = javaClass<Callback_int_int>())
         public fun toNative(fn: ((Int) -> Int)?, context: ToNativeContext): Callback_int_int? {
             return if (fn != null) object : Callback_int_int { override fun callback(param: Int): Int = fn(param) } else null
@@ -34,8 +36,19 @@ public trait LibFfiBench {
     fun kffis_func_callback_int(cb: KTCallBack.Callback_int_int): Int
 }
 
-fun LibFfiBench.kffis_func_callback_int_w(cb: (Int) -> Int): Int =
-        kffis_func_callback_int(object : KTCallBack.Callback_int_int { override fun callback(param: Int): Int = cb(param) })
+public class callback1: KTCallBack.Callback_int_int {
+    override fun callback(param: Int): Int {
+        return param % 42 + 1
+    }
+}
+
+public class callback2(val fn: (Int) -> Int): KTCallBack.Callback_int_int {
+    override fun callback(param: Int): Int = fn(param)
+}
+
+fun LibFfiBench.kffis_func_callback_int(cb: (Int) -> Int): Int =
+        kffis_func_callback_int(callback2(cb))
+//        kffis_func_callback_int(object : KTCallBack.Callback_int_int() { override fun callback(param: Int): Int = cb(param) })
 
 fun measureAll(repeats: Int) {
     val libffi = jnr.ffi.LibraryLoader.create(javaClass<LibFfiBench>()).load("kotlinffibench")
@@ -43,7 +56,11 @@ fun measureAll(repeats: Int) {
 
     println("JNR CB results ($repeats repeats, calibrated to ${calibration}us)")
 
-    val callback = { (a: Int) -> a % 42 + 1  }
+    val callback_stat = callback1()
+    val callback_dyn = { (a: Int) -> a % 42 + 1  }
+    val callback_ds = callback2(callback_dyn)
 
-    println("callback->int: ${assert_equals_measure({ libffi.kffis_func_callback_int_w(callback) }, 1, repeats, calibration)}us")
+    println("callback->int (static): ${assert_equals_measure({ libffi.kffis_func_callback_int(callback_stat) }, 1, repeats, calibration)}us")
+    println("callback->int (lamba): ${assert_equals_measure({ libffi.kffis_func_callback_int(callback_ds) }, 1, repeats, calibration)}us")
+    println("callback->int (dynamic): ${assert_equals_measure({ libffi.kffis_func_callback_int(callback_dyn) }, 1, repeats, calibration)}us")
 }
